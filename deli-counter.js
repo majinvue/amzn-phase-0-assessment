02ab8c64ab47d6ae7a8a10bb5a0f2e4c4227fc42
lloydvue <lloydvue94@gmail.com>
2022-04-10 19:39:08 -0700

    Assignment completed

--- deli-counter.js
+++ deli-counter.js
@@ -1,6 +1,29 @@
 // 1. Write your functions here
+function takeANumber(katzDeli, name) {
+    katzDeli.push(name);
+    return `Welcome, ${name}. You are number ${katzDeli.length} in line.`
+}
 
+function nowServing(katzDeli) {
+    if (katzDeli.length > 0) {
+        return "Currently serving" + katzDeli.shift() + ".";
+      } else {
+        return "There is nobody waiting to be served."
+    }
+}
 
+function currentLine(katzDeli) {
+    let output = []
+    if (katzDeli.length > 0) {
+        for(let i = 0; i < katzDeli.length; i++) {
+            output += (i + 1) + "." + katzDeli[i] + ", ";
+        }
+        output =output.slice(0, output.length - 2);
+        return "The line is currently: " + output;
+      } else {
+        return "The line is currently empty";
+    } 
+}
 // 2. Example Usage
 
 // const katzDeli = []
